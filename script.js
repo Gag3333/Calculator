72ad11e7bc96a2833e6fd282ab688a1560f28e47
Arsen Mkrtchyan <arsen_mkrtchyan@epam.com>
2024-11-16 23:09:35 +0400

    Make similar to windows calculator

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-function angam() {
-    let t1 = document.getElementById("tiv1").value
-    let t2 = document.getElementById("tiv2").value
-    document.getElementById("pat").innerHTML = t1*t2;
-}
+let calculator = {
+    naxord: 0,
+    operator: null,
+    getInput: function()
+    {
+        return document.getElementById("numpad").value;
+    },
+    setInput: function(input)
+    {
+        document.getElementById("numpad").value = input;
+    },
+    onNum: function(num)
+    {
+        let input = this.getInput() + num;
+        this.setInput(input);
+    },
+    onOp: function(op)
+    {
+        this.operator = op;
+        this.naxord = this.getInput();
+        this.setInput('');
+    },
+    onEq: function()
+    {
+        let cur = parseFloat(this.getInput());
+        let prev = parseFloat(this.naxord);
+        let answer = '';
 
-function baj() {
-    let t1 = document.getElementById("tiv1").value
-    let t2 = document.getElementById("tiv2").value
-    document.getElementById("pat").innerHTML = t1/t2;
-}
+        if (this.operator == '+')
+        {
+            answer = cur + prev;
+        }
+        else if(this.operator == '-')
+        {
+            //
+        }
 
-function han() {
-    let t1 = document.getElementById("tiv1").value
-    let t2 = document.getElementById("tiv2").value
-    document.getElementById("pat").innerHTML = t1-t2;
-}
-
-function gum() {
-    let t1 = parseFloat(document.getElementById("tiv1").value);
-    let t2 = parseFloat(document.getElementById("tiv2").value);
-    document.getElementById("pat").innerHTML = t1 + t2;
-}
+        this.setInput(answer);
+    }
+};
