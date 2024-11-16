b987230905c7c7a304d84aea0ef0d5a0e7b2310d
Gag <144773768+Gag3333@users.noreply.github.com>
2024-11-16 10:52:59 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function angam() {
+    let t1 = document.getElementById("tiv1").value
+    let t2 = document.getElementById("tiv2").value
+    document.getElementById("pat").innerHTML = t1*t2;
+}
+
+function baj() {
+    let t1 = document.getElementById("tiv1").value
+    let t2 = document.getElementById("tiv2").value
+    document.getElementById("pat").innerHTML = t1/t2;
+}
+
+function han() {
+    let t1 = document.getElementById("tiv1").value
+    let t2 = document.getElementById("tiv2").value
+    document.getElementById("pat").innerHTML = t1-t2;
+}
+
+function gum() {
+    let t1 = document.getElementById("tiv1").value
+    let t2 = document.getElementById("tiv2").value
+    document.getElementById("pat").innerHTML = t1+t2;
+}
