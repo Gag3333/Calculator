d1ff8a81551c99fc624e961460bc2710ad60ccd1
Arsen Mkrtchyan <arsen_mkrtchyan@epam.com>
2024-11-24 20:52:07 +0400

    fix formatting and mins

--- script.js
+++ script.js
@@ -26,21 +26,18 @@ let calculator = {
         let prev = parseFloat(this.naxord);
         let answer = '';
 
-        if (this.operator == '+')
-        {
-            answer = cur + prev;
+        if (this.operator == '+') {
+            answer = prev + cur;
         }
-        else if(this.operator == '-')
-        {
-            answer = cur - prev;
-        }         if (this.operator == '*')
-            {
-                answer = cur * prev;
-            }
-            else if(this.operator == '/')
-                {
-                    answer = prev / cur;
-                }
+        else if (this.operator == '-') {
+            answer = prev - cur;
+        } 
+        else if (this.operator == '*') {
+            answer = cur * prev;
+        }
+        else if (this.operator == '/') {
+            answer = prev / cur;
+        }
 
         this.setInput(answer);
     }
